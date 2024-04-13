a67599ae343a27e6a078e42a7d391e5c243d1348
XJIEI5 <egorchernov37@yandex.ru>
2024-04-13 20:08:19 +0300

    timeouts now separate for users

--- internal/storage/timeout_handle.go
+++ internal/storage/timeout_handle.go
@@ -13,11 +13,21 @@ func (s *storage) handleSetTimeouts(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	bearerToken := r.Header.Get("Authorization")
+	if bearerToken == "" {
+		http.Error(w, `no header "Authorization"`, http.StatusBadRequest)
+		return
+	}
+	userId, err := getUserId(bearerToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	timeouts := struct {
 		Value map[string]int `json:"timeout"`
 	}{}
-	err := json.NewDecoder(r.Body).Decode(&timeouts)
+	err = json.NewDecoder(r.Body).Decode(&timeouts)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
@@ -29,7 +39,11 @@ func (s *storage) handleSetTimeouts(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("there is no operand '%s'", key), http.StatusBadRequest)
 			return
 		}
-		s.timeouts[key] = value
+		err := storeTimeout(s.db, key, value, userId)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 	w.WriteHeader(http.StatusOK)
 }
